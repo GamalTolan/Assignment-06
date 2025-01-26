e429af6deed3e00c2022342d4867e2c310c52d3b
hp <hp@LAPTOP-RB8GVLBO>
2025-01-26 17:22:43 +0200

    Add project files.

---
+++ Assignment 06/Duration.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_06
+{
+    public abstract class Duration
+    {
+        public abstract int Hours { get; set; }
+        public abstract int Minutes { get; set; }
+        public abstract int Seconds { get; set; }
+
+        public abstract void TheTime();
+
+        public override string ToString()
+        {
+            if (Hours > 0)
+                return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+            if (Minutes > 0)
+                return $"Minutes: {Minutes}, Seconds: {Seconds}";
+            return $"Seconds: {Seconds}";
+        }
+
+
+        public class Time : Duration
+        {
+            public override int Hours { get; set; }
+            public override int Minutes { get; set; }
+            public override int Seconds { get; set; }
+
+            public Time(int hours, int minutes, int seconds)
+            {
+                Hours = hours;
+                Minutes = minutes;
+                Seconds = seconds;
+            }
+
+            public Time(int Seconds)
+            {
+                Hours = Seconds / 3600;
+                Minutes = (Seconds % 3600) / 60;
+                Seconds = Seconds % 60;
+            }
+            public override void TheTime()
+            {
+                Minutes += Seconds / 60;
+                Seconds %= 60;
+                Hours += Minutes / 60;
+                Minutes %= 60;
+            }
+
+            public static Time operator +(Time t1, Time t2)
+            {
+                return new Time ( t1.Hours + t2.Hours, t1.Minutes + t2.Minutes, t1.Seconds + t2.Seconds );
+            }
+
+            public static Time operator +(Time t, int Seconds)
+            {
+               return new Time(t.Hours*3600+ t.Minutes*60+ t.Seconds + Seconds);
+            }
+
+            public static Time operator +(int Seconds, Time t)
+            {
+                return new Time(t.Hours * 3600 + t.Minutes * 60 + t.Seconds + Seconds);
+            }
+
+            public static Time operator ++(Time t)
+            {
+                return new Time(t.Hours, t.Minutes + 1, t.Seconds );
+            }
+
+            public static Time operator --(Time t)
+            {
+                return new Time(t.Hours, t.Minutes - 1, t.Seconds);
+            }
+
+            public static Time operator -(Time t1, Time t2)
+            {
+                return new Time(t1.Hours - t2.Hours, t1.Minutes - t2.Minutes, t1.Seconds - t2.Seconds);
+            }
+
+            public static bool operator > (Time t1, Time t2)
+            {
+                if (t1.Hours > t2.Hours)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes > t2.Minutes)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds > t2.Seconds)
+                    return true;
+                else
+                return false;
+            }
+
+            public static bool operator <(Time t1, Time t2)
+            {
+                if (t1.Hours < t2.Hours)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes < t2.Minutes)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds < t2.Seconds)
+                    return true;
+                else
+                    return false;
+            }
+
+            public static bool operator <=(Time t1, Time t2)
+            {
+                if (t1.Hours < t2.Hours)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes < t2.Minutes)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds < t2.Seconds)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds == t2.Seconds)
+                    return true;
+                else
+                    return false;
+            }
+
+            public static bool operator >=(Time t1, Time t2)
+            {
+                if (t1.Hours > t2.Hours)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes > t2.Minutes)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds > t2.Seconds)
+                    return true;
+                else if (t1.Hours == t2.Hours && t1.Minutes == t2.Minutes && t1.Seconds == t2.Seconds)
+                    return true;
+                else
+                    return false;
+            }
+
+            
+        }
+    }
+
+    
+}
---
+++ Assignment 06/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Assignment_06.Duration;
+
+namespace Assignment_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1
+
+            Time D1 = new Time(1, 10, 15);
+            Console.WriteLine(D1.ToString());
+
+            Time D2 = new Time(3600);
+            Console.WriteLine(D2.ToString());
+
+            Time D3 = new Time(7800);
+            //Console.WriteLine(D3.ToString());
+
+            //Time D4 = new Time(666);
+            //Console.WriteLine(D4.ToString());
+
+            Console.WriteLine("======================================");
+
+            #endregion
+
+            #region 2
+
+            //D3 = D1 + D2;
+            //Console.WriteLine(D3.ToString());
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine(D3);
+
+            //D3 = 666 + D3;
+            //Console.WriteLine(D3);
+
+            //D3 = ++D1;
+            //Console.WriteLine(D3);
+
+            //D3=--D1;
+            //Console.WriteLine(D3);
+
+            //D1 = D1 - D2;
+            //Console.WriteLine(D1);
+
+            if(D1 > D2)
+            {
+                Console.WriteLine("D1 is greater than D2");
+            }
+            else
+            {
+                Console.WriteLine("D1 is not greater than D2");
+            }
+
+            if(D1 <= D2)
+            {
+                Console.WriteLine("D1 is less than or equal to D2");
+            }
+            else
+            {
+                Console.WriteLine("D1 is not less than or equal to D2");
+            }
+
+            // if (D1) ????
+
+            //DateTime Obj = (DateTime) D1 ?
+
+
+
+
+            #endregion
+        }
+    }
+}
